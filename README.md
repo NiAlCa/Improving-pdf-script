b0a6cf5f1bc275421a43e3e154e7c7aef3115c3f
Nicolae <casiean2@gmail.com>
2024-02-07 17:50:33 +0100

    mejorando script para que cuando cree excel coloree los pedidos de viaje de un mismo color

---
+++ README.md
@@ -0,0 +1,9 @@
+Script for Improving logistics 
+
+An advanced executable script designed to efficiently process PDFs from clients, 
+transforming the data into a well-structured Excel table. This tool is specifically
+tailored for warehouse management, streamlining the organization of inventory and resources.
+By automating the data extraction and organization process, the script significantly reduces 
+the time usually spent on manual data entry. More importantly, it minimizes the risk of human error,
+ensuring accuracy and reliability in inventory management. This technological solution thus enhances 
+overall operational efficiency in warehouse settings.
--- index.js
+++ index.js
@@ -1,19 +1,32 @@
 const fs = require('fs');
 const pdfParse = require('pdf-parse');
 const ExcelJS = require('exceljs');
+const path = require('path');
 
-// Patrones de Regex para extracción de datos
+const folderPath = process.cwd();
+
+//Patrones de Regex
 const regexInformacionCompleta = /(\d+)(\d{2}\/\d{2}\/\d{4} \d{2}:\d{2})([A-Za-z0-9\s]+[A-Za-zÀ-ÿ\u00f1\u00d1]+\s+)/;
 const regexDestino = /\b[0-9]{4}(\s[A-Za-z]+)+\b/;
 const regexDestinoReal = /ESP\d\d\d\d\d ([A-Za-z]+( [A-Za-z]+)+)/;
-const regexNumeroDeViaje = /\b\d{4}[A-Za-z]{4}\d{4}\b/;
-const regexNumeroDePedido = /\d\d\d\d\d\d\d\d\/\d/;
+const regexPedidoViaje = /(\d{8}\/\d)([a-zA-Z0-9]+)/; 
 const regexMatricula = /^[0-9]{1,4}(?!.*(LL|CH))[BCDFGHJKLMNPRSTVWXYZ]{3}/;
+const regexAlmacen1 = /8412353000187/;
+const regexAlmacen2 = /8412353000170/;
+const regexAlmacen3 = /8412353000033/;
 
-// Función para leer el PDF y extraer el texto
+// Función para leer el PDF
 async function readPdf(filePath) {
     const dataBuffer = fs.readFileSync(filePath);
     return await pdfParse(dataBuffer);
+}
+
+// Función para detectar el almacén 
+function detectAlmacen(text) {
+    if (regexAlmacen1.test(text)) return '8412353000187';
+    if (regexAlmacen2.test(text)) return '8412353000170';
+    if (regexAlmacen3.test(text)) return '8412353000033';
+    return ''; 
 }
 
 // Función para procesar el texto del PDF y convertirlo en una estructura de datos
@@ -21,6 +34,10 @@ function processData(text) {
     const lines = text.split('\n');
     const camiones = [];
     let currentCamion = {};
+
+    // Detectar el almacén para este PDF
+    const almacenDetectado = detectAlmacen(text);
+
     lines.forEach(line => {
         let matchResult;
 
@@ -29,26 +46,74 @@ function processData(text) {
             currentCamion.fechaRecogida = matchResult[2];
             currentCamion.delegacion = matchResult[3].trim();
         } else if ((matchResult = line.match(regexDestino)) || (matchResult = line.match(regexDestinoReal))) {
-            if (currentCamion.destino) camiones.push(currentCamion);
+            if (currentCamion.destino) {
+                currentCamion.almacen = almacenDetectado; 
+                camiones.push(currentCamion);
+            }
             currentCamion = { destino: matchResult[0] };
-        } else if ((matchResult = line.match(regexNumeroDePedido))) {
-            currentCamion.numeroDePedido = matchResult[0];
-        } else if ((matchResult = line.match(regexNumeroDeViaje))) {
-            currentCamion.numeroDeViaje = matchResult[0];
+        } else if ((matchResult = line.match(regexPedidoViaje))) {
+            currentCamion.numeroDePedido = matchResult[1];
+            currentCamion.numeroDeViaje = matchResult[2];
         } else if ((matchResult = line.match(regexMatricula))) {
             currentCamion.matricula = matchResult[0];
         }
     });
 
-    if (Object.keys(currentCamion).length !== 0) camiones.push(currentCamion);
+    if (Object.keys(currentCamion).length !== 0) {
+        currentCamion.almacen = almacenDetectado; 
+        camiones.push(currentCamion);
+    }
 
     return camiones;
 }
 
-// Función para crear un archivo Excel con los datos de los camiones
+function tieneCoincidencias(numeroDeViaje, camiones) {
+    return camiones.filter(camion => camion.numeroDeViaje === numeroDeViaje).length > 1;
+}
+const colors = [
+    'FFFF00', // Amarillo
+    'FF0000', // Rojo
+    '00FF00', // Verde
+    '0000FF', // Azul
+    'FF00FF', // Magenta
+    '00FFFF', // Cian
+    'FF8000', // Naranja
+    '8000FF', // Morado
+    'A52A2A', // Marrón
+    '808080', // Gris
+    '000000', // Negro
+    'FFFFFF', // Blanco
+    'FFC0CB', // Rosa
+    'FA8072', // Salmón
+    'FFE5B4', // Melocotón
+    '808000', // Verde oliva
+    '000080', // Azul marino
+    'C8A2C8', // Lila
+    'FFD700', // Amarillo vibrante
+    'DC143C', // Rojo vibrante
+    '00FF7F', // Verde vibrante
+    '007FFF', // Azul vibrante
+    'FF0080', // Magenta vibrante
+    '00FFFF', // Cian vibrante
+  
+
+
+];
+
+// Función para asignar un color único a cada número de pedido
+function getColorForPedido(numeroDeViaje, pedidoColorsMap) {
+    if (!pedidoColorsMap.has(numeroDeViaje)) {
+        // Asignar un nuevo color de la lista, rotando si es necesario
+        const color = colors[pedidoColorsMap.size % colors.length];
+        pedidoColorsMap.set(numeroDeViaje, color);
+    }
+    return pedidoColorsMap.get(numeroDeViaje);
+}
+
 async function createExcelFile(camiones, outputPath) {
     const workbook = new ExcelJS.Workbook();
     const sheet = workbook.addWorksheet('Camiones');
+    const pedidoColorsMap = new Map(); // Mapa para asociar número de pedido con color
 
     // Añadir encabezados de columna
     sheet.columns = [
@@ -59,35 +124,57 @@ async function createExcelFile(camiones, outputPath) {
         { header: 'Fecha Recogida', key: 'fechaRecogida', style: { numFmt: 'dd/mm/yyyy hh:mm' } },
         { header: 'Pallets', key: 'pallets' },
         { header: 'Delegación', key: 'delegacion' },
-        // Agregar más columnas si es necesario
+        { header: 'Almacén', key: 'almacen' },
     ];
 
-    // Añadir datos a las filas
     camiones.forEach(camion => {
-        sheet.addRow(camion);
+        const row = sheet.addRow(camion);
+        const color = getColorForPedido(camion.numeroDeViaje, pedidoColorsMap);
+        row.eachCell(cell => {
+            cell.style.fill = {
+                type: 'pattern',
+                pattern: 'solid',
+                fgColor: { argb: color }
+            };
+        });
     });
 
     // Guardar el archivo Excel
     await workbook.xlsx.writeFile(outputPath);
+    console.log('Archivo Excel creado con éxito.');
 }
 
-// Ruta al archivo PDF
-const pdfPath = 'file.pdf';
+// Función para procesar todos los PDFs en una carpeta
+async function processAllPdfsInFolder(folderPath) {
+const files = fs.readdirSync(folderPath);
+const pdfFiles = files.filter(file => path.extname(file).toLowerCase() === '.pdf');
 
-// Procesar PDF y crear archivo Excel
-readPdf(pdfPath)
-    .then(data => {
+let allCamiones = [];
+
+for (let file of pdfFiles) {
+    const filePath = path.join(folderPath, file);
+    try {
+        const data = await readPdf(filePath);
         const camiones = processData(data.text);
-        return createExcelFile(camiones, 'camiones.xlsx');
-    })
-    .then(() => {
-        console.log('Archivo Excel creado con éxito.');
-    })
-    .catch(err => {
-        console.error('Error:', err);
-    });
+        allCamiones = allCamiones.concat(camiones);
+    } catch (err) {
+        console.error('Error procesando el archivo:', file, err);
+    }
+}
+return allCamiones;
 
 
 
+}
 
+
+
+// Procesar todos los PDFs en la carpeta y crear archivo Excel
+processAllPdfsInFolder(folderPath)
+.then(allCamiones => {
+return createExcelFile(allCamiones, path.join(folderPath, 'camiones.xlsx'));
+})
+.catch(err => {
+console.error('Error:', err);
+});
 
